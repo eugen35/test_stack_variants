da73b7e9d54877b5033d1493cb8b4ef52050071c
_meugen_ <eugen35@mail.ru>
2016-07-01 19:48:42 +0500

    Размышляю над методами пагинатора.... А точнее уже над api remotesource и hashstorage (скорее всего последнее должно быть внутри remotesource).... А api paginatora должно быть очень простым

--- js/paginationAndScroll/partListViewer.js
+++ js/paginationAndScroll/partListViewer.js
@@ -1,10 +1,11 @@
 export default class PartListViewer {
-  constructor({ remoteSource, hashedRecords, currentPageRecords, currentPageRecordsCount, sort, filter }) {
+  constructor({ remoteSource, hashedRecords, currentPageRecords, currentPageRecordsCount, sort, filter, user }) {
     this.remoteSource = remoteSource
     this.hashedRecords = hashedRecords
     this.currentPageRecords = currentPageRecords
     this.currentPageRecordsCount = currentPageRecordsCount
     this.sort = sort
     this.filter = filter
+    this.user = user
   }
 }
