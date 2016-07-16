e4c9788315e1e5b0f15440fec9d73bb42b19b8a6
_meugen_ <eugen35@mail.ru>
2016-07-16 21:06:51 +0500

    Использование Istanbul настроил

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'js/**/*.js': ['babel', 'commonjs']
+      'js/**/*.js': ['babel', 'commonjs', 'coverage']
     },
 
     babelPreprocessor: {
@@ -44,8 +44,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
